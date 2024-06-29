ffc6bf20a94257e9f468615cf84b024e2b6beb56
romanvcode <roman.v.ua@gmail.com>
2024-06-29 14:33:49 +0300

    Add project files.

---
+++ ApartmentsMarketplace.WebAPI/Program.cs
@@ -0,0 +1,24 @@
+using ApartmentMarketplace.Core.Services;
+using ApartmentMarketplace.Core.RepositoryContracts;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHsts();
+app.UseHttpsRedirection();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
